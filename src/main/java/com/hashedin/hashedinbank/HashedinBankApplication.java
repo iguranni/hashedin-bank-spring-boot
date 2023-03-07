4f652f4ea6196e9d3793c194d84a0a85c3700b48
iguranni <iguranni@deloitte.com>
2023-03-08 00:38:36 +0530

    Project structure setup

---
+++ src/main/java/com/hashedin/hashedinbank/HashedinBankApplication.java
@@ -0,0 +1,13 @@
+package com.hashedin.hashedinbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashedinBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashedinBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hashedin/hashedinbank/HashedinBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashedin.hashedinbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashedinBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
